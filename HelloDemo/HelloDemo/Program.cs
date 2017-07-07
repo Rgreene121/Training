e4d28f878378d519a06b3abcbb8580bf9fa0575a
rgreene121 <rabib.greene@gmail.com>
2017-07-07 15:48:51 -0400

    worked on hello Demo and userInput

---
+++ HelloDemo/HelloDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace HelloDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+            //Console.ReadKey();
+
+            Console.WriteLine("We r here to become developers");
+            Console.WriteLine("today is Friday");
+            //Console.WriteLine("today is Friday");
+            //Console.ReadKey();
+
+            //ReadKey();
+            WriteLine("We will go to lunch soon!!");
+            Write("now \n and forever\n");
+            Write("today");
+            ReadKey();
+
+
+
+
+        }
+
+    }
+}
---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("What is your name??");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " How are you today?");
+            Console.ReadLine();
+
+            Console.WriteLine("I am glad to hear that");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age??");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("You are a {0} year old man", age);
+            Console.WriteLine(" {0} you are an {1} year old guy, am I right?", name, age, age);
+            Console.ReadLine();
+
+            Console.WriteLine("Are you going out tonight:", age);
+            string goingOut = Console.ReadLine();
+
+            Console.WriteLine($"My name is {name} I am {age} and {goingOut} of course");
+            Console.ReadLine();
+
+
+
+
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
