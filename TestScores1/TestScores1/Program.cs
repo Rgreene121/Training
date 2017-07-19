35f8c3992e7f640b6fa609249d14b012081ebc38
rgreene121 <rabib.greene@gmail.com>
2017-07-19 10:18:07 -0400

    Random operator

---
+++ TestScores1/TestScores1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScores1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idNum;
+            int lowNum = 1;
+            int highNum = 100;
+
+            Console.WriteLine("Enter a valid test score");
+            string valNum = Console.ReadLine();
+            int.TryParse(valNum, out idNum);
+
+            while (idNum < lowNum || idNum >= highNum)
+            {
+                Console.WriteLine($"The Id number you entered {valNum} \n" + $"must be between {lowNum} and {highNum} \n Try again");
+                valNum = Console.ReadLine();
+                int.TryParse(valNum, out idNum);
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
