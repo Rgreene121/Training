ab34e395a0a2040cd1d33ccd17903da586d42bfa
rgreene121 <rabib.greene@gmail.com>
2017-07-19 09:22:36 -0400

    how to add to a class

---
+++ TestScores/TestScores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int idNum;
+            int sum = 0;
+            int total = 100;
+            //Array  = ArraySegment();
+
+            int rNum = rand.Next(1, 100);
+            int realNum;
+
+            for (int i = 0; i <= 100; i++)
+
+            {
+                Console.WriteLine("Enter a Number");
+                string score = Console.ReadLine();
+                int.TryParse(score, out total);
+            }
+            //while(idNum < sum || idNum >= total)
+            if (sum == 101)
+            {
+                Console.WriteLine("You must enter a valid score");
+                score = Console.ReadLine();
+                int.TryParse(score, out total);
+            }
+            else if (total <= 100 )
+            {
+                Console.WriteLine("You are on the right track");
+            }
+            //while (Get(input) != Sentinel)
+            //{
+            //    Process(input);
+            //}
+
+
+        }
+            
+
+    }
+}
---
+++ guessingGame1/guessingGame1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessingGame1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int rNum = rand.Next(1, 6);
+            int realNum;
+            //int bNum = rand.Next(22, >= 100; ++ 1);
+
+            Console.WriteLine("Enter a number between 1 and 5");
+            string numGuess = Console.ReadLine();
+
+            int.TryParse(numGuess, out realNum);
+
+            //while (realNum < 0 || realNum > 6)
+            //{
+            //    Console.WriteLine("Your must guess between 1 and 6 \n Guess again");
+
+            //    numGuess = Console.ReadLine();
+
+            //    int.TryParse(numGuess, out realNum);
+            
+            //}
+            while (realNum != rNum)
+            {
+                Console.WriteLine($"Your guess of {numGuess} was not correct");
+                Console.ReadLine();
+
+                numGuess = Console.ReadLine();
+
+                int.TryParse(numGuess, out realNum);
+            }
+            Console.WriteLine($"Your guess of {numGuess} was correct...Hooray!!");
+        }
+    }
+}
