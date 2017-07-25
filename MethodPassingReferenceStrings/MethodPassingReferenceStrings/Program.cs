ab1571640cdb70de17a0fb4677f150a9f318bae0
rgreene121 <rabib.greene@gmail.com>
2017-07-25 09:13:02 -0400

    Variable swap with names

---
+++ MethodPassingReferenceStrings/MethodPassingReferenceStrings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPassingReferenceStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string raven;
+            string partner = "Kofi";
+            raven = "Hello my name is Harry Potter";
+            Console.WriteLine($"Hello {raven}, who is your {partner}");
+            Console.WriteLine($"Hello {partner} you are the second player");
+            //int.TryParse(partner, out secondPlayer);
+            
+            Console.ReadLine();
+        }
+        public static void NewName(string kim)
+        {
+            kim = newName();
+            Console.WriteLine($"I thought your name was Harry?");
+            Console.ReadLine();
+
+
+        }
+
+        private static string newName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MethodPassingReferenceStrings2/MethodPassingReferenceStrings2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPassingReferenceStrings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("player 1 ...What is your first name??");
+            string player1 = Console.ReadLine();
+            NameChange(player1);
+            Console.WriteLine($"in the main method...the name of player 1 is {player1}");
+            NameTransform(ref player1);
+            Console.WriteLine($"Inside of the very main method the value of player 1 is {player1}");
+
+            Console.ReadLine();
+
+
+        }
+        public static void NameChange(string origPlayer1)
+        {
+            origPlayer1 = "new Player 1";
+            Console.WriteLine($"Inside of the Name change method, the name of Player 1 \n is {origPlayer1}");
+
+        }
+        public static void NameTransform(ref string player1)
+        {
+            player1 = "I am the mother of Dragons!!";
+            Console.WriteLine($"today {player1}");
+
+        }
+    }
+}
