72e8b27ace5ec26a2659145d08ec2fd09c1aade2
rgreene121 <rabib.greene@gmail.com>
2017-07-25 11:14:56 -0400

    Array Sting with Letters and array Int

---
+++ PassingDataTypes/PassingDataTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInt = { 1, 5, 12, 66, 27 };
+            string [] nameArray = {"Nancy Drew",  "Columbo",  "The Hardy Boys"};
+
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                Console.WriteLine("The value of the ArrayInt array is " + arrayInt[i]);
+
+            }
+            Console.WriteLine("we are now out of the loop");
+            Console.ReadLine();
+            MaxArray(arrayInt);
+            SumArray(arrayInt);
+            ListNames(nameArray);
+        }   
+        public static void SumArray(int [] arrayVals)
+        {
+            int sumation = arrayVals.Sum();
+            Console.WriteLine($"The sum of the array is {sumation}");
+            Console.ReadLine();
+        }
+        public static void MaxArray(int [] intArray)
+        {
+            int maxVal = intArray.Max();
+            Console.WriteLine($"The max value of the array is {maxVal}");
+            Console.ReadLine();
+        }
+        public static void ListNames(string [] Names)
+        {
+            //string allNames = Names.
+            for (int k = 0; k < Names.Length; k++)
+            {
+                Console.WriteLine("The names of the ArrayNames array are " + Names[k]);
+            }
+            Console.ReadLine();
+
+        }
+        
+    }
+}
