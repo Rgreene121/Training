fb8145c9df60773fbb2a7c3d405b0b4d1987267a
rgreene121 <rabib.greene@gmail.com>
2017-07-27 12:14:32 -0400

    Passing Variables

---
+++ ReturnValurPractice2/ReturnValurPractice2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnValurPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1 = 200;
+            int number2 = 300;
+
+            int complete = TotalNumber(number1, number2);
+
+            Console.WriteLine(complete);
+
+            Console.ReadLine();
+
+            //TotalNumber(number1, number2);
+        }
+        private static int TotalNumber(int n1, int n2)
+        {
+            int product = n1 * n2;
+            return product;
+
+        }
+    }
+}
