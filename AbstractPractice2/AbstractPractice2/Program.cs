20c83453f95333bc37c9b2585a9dcaf178b777a2
rgreene121 <rabib.greene@gmail.com>
2017-08-01 11:58:35 -0400

    Abstract Pillar

---
+++ AbstractPractice2/AbstractPractice2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marvel spidey = new Marvel();
+
+            int bravery;
+            string speed;
+
+            speed = spidey.Justice();
+            Console.WriteLine(speed);
+
+            bravery = spidey.Scheme();
+            Console.WriteLine(bravery);
+            Console.ReadLine();
+
+        }
+    }
+}
