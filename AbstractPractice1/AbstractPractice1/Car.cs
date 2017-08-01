9e650aa7141263cb3ef208abd4450f18fac6cbf6
rgreene121 <rabib.greene@gmail.com>
2017-08-01 11:33:18 -0400

    Abstract Value

---
+++ AbstractPractice1/AbstractPractice1/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPractice1
+{
+    class Car
+    {
+        public int mileage = 60;
+
+        public string color = "Blue";
+
+        private string formula = "a + b";
+
+
+
+        public int GetMileage()
+        {
+            return mileage;
+        }
+
+        public string GetColor()
+        {
+            return color;
+
+        }
+        private string GetEngineformula()
+        {
+            return formula;
+
+        }
+        public string DisplayEngineFormula()
+        {
+            return GetEngineformula();
+        }
+    }
+}
---
+++ AbstractPractice1/AbstractPractice1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car drive = new Car();
+
+            Console.WriteLine($"Car mileage is {drive.GetMileage()}");
+
+            Console.WriteLine($"Car color is {drive.GetColor()}");
+
+            Console.WriteLine($"The secret formula is {drive.DisplayEngineFormula()}");
+
+            Console.ReadLine();
+        }
+    }
+}
